a78074fdf7267b83b41a3cff2ccf119fd4ee7585
Rico Hermsen <rico@jrny.nl>
2025-08-29 08:48:40 +0200

    feat: change font

--- config.h
+++ config.h
@@ -5,8 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-// static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
-static char *font = "JetBrainsMono NF:pixelsize=12:antialias=true:autohint=true";
+//Hack Nerd Font Mono
+static char *font = "Hack Nerd Font Mono:pixelsize=12:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
